31ed31b3794d2caeddfa48805bceb00ef2fc599b
Izac <izikki11499@gmail.com>
2022-05-30 13:55:25 -0700

    Made HTML calculator, styled a bit added more operatins user can use, percents, absolute value

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
+const posNeg = (a) => a * -1;
+const percentage = (a) => a / 100;
 
 //Operate, take in an operator, two numbers and evaluate the two numbers with operation
 
@@ -11,9 +13,7 @@ const divide = (a, b) => a / b;
  * how to determine which operation to perform on the two numbers?
  * operator is going to be determined by which button user presses, so how to apply that button to these numbers and operate with them
  */
-
 const operate = (operation, a, b) => {
-    
     //perform operation on numbers depending on what operation is passed
     switch(operation) {
         case 'add':
@@ -26,5 +26,5 @@ const operate = (operation, a, b) => {
             return multiply(a,b);
         case 'divide':
             return divide(a,b);
-      }
-};
+      }//end switch
+};//end operate()
