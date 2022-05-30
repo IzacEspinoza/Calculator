fa051006e2e02fd7f62e1ad0774410dc1edc3349
Izac <izikki11499@gmail.com>
2022-05-29 22:35:14 -0700

    Created logic for the operate() function, using switch statment

--- app.js
+++ app.js
@@ -1,3 +1,30 @@
 //operations
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
 
 //Operate, take in an operator, two numbers and evaluate the two numbers with operation
+
+/**
+ * operate(operation, a, b)
+ * how to determine which operation to perform on the two numbers?
+ * operator is going to be determined by which button user presses, so how to apply that button to these numbers and operate with them
+ */
+
+const operate = (operation, a, b) => {
+    
+    //perform operation on numbers depending on what operation is passed
+    switch(operation) {
+        case 'add':
+          return add(a,b);
+          break;
+        case 'subtract':
+          return subtract(a,b);
+          break;
+        case 'multiply':
+            return multiply(a,b);
+        case 'divide':
+            return divide(a,b);
+      }
+};
