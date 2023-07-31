edbedf1fb3b1b12433f46667a4c7d03cf51c267d
Izac <izikki11499@gmail.com>
2023-07-31 16:36:39 -0700

    wrote the basic functions of a calculator

---
+++ script.js
@@ -0,0 +1,9 @@
+//Basic functins of calculator
+//additon
+const add = (a, b) => {return a + b;}
+//subtraction
+const subtract = (a, b) => {return a - b};
+//multiplication
+const multiply = (a, b) => {return a * b;}
+//division
+const divide = (a, b) => {return a / b;}
