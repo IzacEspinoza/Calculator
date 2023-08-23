51b353fa57096ab06b369505c549a608dc225178
Izac <izikki11499@gmail.com>
2023-08-22 20:19:54 -0700

    planning out the main calculation function

--- script.js
+++ script.js
@@ -7,3 +7,10 @@ const subtract = (a, b) => {return a - b};
 const multiply = (a, b) => {return a * b;}
 //division
 const divide = (a, b) => {return a / b;}
+
+//Calculation function
+//Take in two numbers and an operation, then call from above(depending on the operator used)
+const calculate = (num1, operator, num2) => {
+    //first see what operator was used(+, *, - etc)
+    //then call a function from above with parameters plugged in based on what operation they chose
+};
