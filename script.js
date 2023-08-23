69245c559d1a0ef859d985f3a7b825ea64eeb068
Izac <izikki11499@gmail.com>
2023-08-22 22:12:27 -0700

    wrote a switch that will call an operation function depending on the operator used, and calculates accordingly, right now just to console

--- script.js
+++ script.js
@@ -11,6 +11,28 @@ const divide = (a, b) => {return a / b;}
 //Calculation function
 //Take in two numbers and an operation, then call from above(depending on the operator used)
 const calculate = (num1, operator, num2) => {
+    //save the current value from a single calc(one operation at time)
+    let current = 0;
     //first see what operator was used(+, *, - etc)
     //then call a function from above with parameters plugged in based on what operation they chose
+    switch(operator) {
+        case '*':
+            //call a function
+            console.log(current = multiply(num1, num2));
+            break;
+        case '+': 
+            //call
+            console.log(current = add(num1, num2));
+            break;
+        case '-':
+            //call
+            console.log(current = subtract(num1, num2));
+            break;
+        case '/':
+            //calll
+            console.log(current = divide(num1, num2));
+            break;
+        default:
+            console.log('Sorry couldnt figure it out');
+    }
 };
